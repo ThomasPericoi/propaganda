2aeaacf4955348bbcfd62b685e3757b7ed99277f
Thomas Pericoi <thomaspericoi@gmail.com>
2021-07-25 23:00:30 +0200

    Client | Clients dynamically displayed

--- templates/block-home-clients.php
+++ templates/block-home-clients.php
@@ -5,138 +5,49 @@
             <h2><?php echo get_theme_mod('pt_clients_heading_title', "Interesting") ?></h2>
             <h3><?php echo get_theme_mod('pt_clients_heading_subtitle', "What a title!") ?></h3>
         </div>
-        <div class="logo-grid grid-<?php echo get_theme_mod('pt_clients_general_color', false) ? 'primary' : 'secondary'; ?> clients-grid">
-            <div class="grid-item client">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/temp/gmio.svg" class="logo" alt="GMIO Logo">
-                <div class="info">
-                    <div class="domains">
-                        <div class="domain" data-tooltip="Website's revamping">
-                            <i class="fas fa-code"></i>
-                        </div>
-                        <div class="domain" data-tooltip="Maintenance">
-                            <i class="fas fa-hammer"></i>
-                        </div>
-                        <div class="domain" data-tooltip="Visual identity">
-                            <i class="fas fa-paint-brush"></i>
-                        </div>
-                        <div class="domain" data-tooltip="Newsletter">
-                            <i class="fas fa-envelope"></i>
-                        </div>
-                        <div class="domain" data-tooltip="Wordpress">
-                            <i class="fab fa-wordpress-simple"></i>
+        <?php
+        $the_query = new WP_Query(array('post_type' => 'pt_client', 'posts_per_page' => 12));
+        if ($the_query->have_posts()) :
+        ?>
+            <div class="logo-grid grid-<?php echo get_theme_mod('pt_clients_general_color', false) ? 'primary' : 'secondary'; ?> clients-grid">
+                <?php while ($the_query->have_posts()) : $the_query->the_post();
+                    $url = get_post_meta(get_the_ID(), 'client_url', true);
+                    $domains = get_post_meta(get_the_ID(), 'client_domains', true); ?>
+                    <div class="grid-item client">
+                        <img src="<?php echo get_the_post_thumbnail_url() ?>" class="logo" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>">
+                        <div class="info">
+                            <?php
+                            if ($domains) :
+                            ?>
+                                <div class="domains">
+                                    <?php
+                                    foreach ($domains as $field) { ?>
+                                        <div class="domain" data-tooltip="<?php echo $field['domainLabel'] ?>">
+                                            <i class="<?php echo $field['domainIcon'] ?>"></i>
+                                        </div>
+                                    <?php
+                                    }
+                                    ?>
+                                </div>
+                            <?php endif; ?>
+                            <h3 class="name"><?php the_title(); ?></h3>
+                            <div class="links">
+                                <a href="<?php the_permalink(); ?>" class="btn-icon link">
+                                    <i class="fas fa-info"></i>
+                                </a>
+                                <a href="<?php echo $url ?>" rel="external" target="_blank" class="btn-icon link">
+                                    <i class="fas fa-external-link-alt"></i>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                    <h3 class="name">GMIO</h3>
-                    <div class="links">
-                        <a href="/" class="btn-icon link">
-                            <i class="fas fa-info"></i>
-                        </a>
-                        <a href="https://www.gmio.fr/" rel="external" target="_blank" class="btn-icon link">
-                            <i class="fas fa-external-link-alt"></i>
-                        </a>
-                    </div>
-                </div>
+                <?php endwhile;
+                wp_reset_postdata(); ?>
             </div>
-            <div class="grid-item client">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/temp/prezevent.svg" class="logo" alt="Prezevent Logo">
-                <div class="info">
-                    <div class="domains">
-                        <div class="domain" data-tooltip="Code optimization">
-                            <i class="fas fa-code"></i>
-                        </div>
-                        <div class="domain" data-tooltip="Performances">
-                            <i class="fas fa-tachometer-alt"></i>
-                        </div>
-                        <div class="domain" data-tooltip="Maintenance">
-                            <i class="fas fa-hammer"></i>
-                        </div>
-                        <div class="domain" data-tooltip="Content writing">
-                            <i class="fas fa-pen-fancy"></i>
-                        </div>
-                        <div class="domain" data-tooltip="Wordpress">
-                            <i class="fab fa-wordpress-simple"></i>
-                        </div>
-                    </div>
-                    <h3 class="name">Prezevent</h3>
-                    <div class="links">
-                        <a href="/" class="btn-icon link">
-                            <i class="fas fa-info"></i>
-                        </a>
-                        <a href="https://www.prezevent.com/" rel="external" target="_blank" class="btn-icon link">
-                            <i class="fas fa-external-link-alt"></i>
-                        </a>
-                    </div>
-                </div>
+        <?php else : ?>
+            <div>
+                <p><?php echo __('No client... Time to search some!', 'propaganda'); ?></p>
             </div>
-            <div class="grid-item client">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/temp/orsys.png" class="logo" alt="Orsys Logo">
-                <div class="info">
-                    <div class="domains">
-                        <div class="domain" data-tooltip="Teaching">
-                            <i class="fas fa-university"></i>
-                        </div>
-                    </div>
-                    <h3 class="name">Orsys</h3>
-                    <div class="links">
-                        <a href="/" class="btn-icon link">
-                            <i class="fas fa-info"></i>
-                        </a>
-                        <a href="https://orsys.fr/" rel="external" target="_blank" class="btn-icon link">
-                            <i class="fas fa-external-link-alt"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="grid-item client">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/temp/triple-double.svg" class="logo" alt="Triple Double Logo">
-                <div class="info">
-                    <div class="domains">
-                        <div class="domain" data-tooltip="Maintenance">
-                            <i class="fas fa-hammer"></i>
-                        </div>
-                        <div class="domain" data-tooltip="Wordpress">
-                            <i class="fab fa-wordpress-simple"></i>
-                        </div>
-                    </div>
-                    <h3 class="name">Triple Double</h3>
-                    <div class="links">
-                        <a href="/" class="btn-icon link">
-                            <i class="fas fa-info"></i>
-                        </a>
-                        <a href="https://tripledouble.fr/" rel="external" target="_blank" class="btn-icon link">
-                            <i class="fas fa-external-link-alt"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="grid-item client">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/temp/laminuteimmobiliere.svg" class="logo" alt="La Minute Immobilière Logo">
-                <div class="info">
-                    <div class="domains">
-                        <div class="domain" data-tooltip="Website's creation">
-                            <i class="fas fa-code"></i>
-                        </div>
-                        <div class="domain" data-tooltip="Maintenance">
-                            <i class="fas fa-hammer"></i>
-                        </div>
-                        <div class="domain" data-tooltip="Visual identity">
-                            <i class="fas fa-paint-brush"></i>
-                        </div>
-                        <div class="domain" data-tooltip="Wordpress">
-                            <i class="fab fa-wordpress-simple"></i>
-                        </div>
-                    </div>
-                    <h3 class="name">La Minute Immobilière</h3>
-                    <div class="links">
-                        <a href="/" class="btn-icon link">
-                            <i class="fas fa-info"></i>
-                        </a>
-                        <a href="https://laminuteimmobiliere.fr/" rel="external" target="_blank" class="btn-icon link">
-                            <i class="fas fa-external-link-alt"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php endif; ?>
     </div>
 </section>
